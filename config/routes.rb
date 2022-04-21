928135ee7564135077b698e3990dbb1cfc62ab74
Benjamin Deconinck <bdbenboss@hotmail.com>
2022-04-21 20:57:10 +0200

    review create

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,8 @@ Rails.application.routes.draw do
 
   get "restaurants/:id", to: "restaurants#show", as: :restaurant
 
+  get "/restaurants/:restaurant_id/reviews/new", to: "reviews#new", as: :review
+  post "/restaurants/:restaurant_id/reviews", to: "reviews#create", as: :restaurants_reviews
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
