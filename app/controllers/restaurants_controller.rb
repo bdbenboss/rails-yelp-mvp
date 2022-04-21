a2db6aae864a0fdee352fee62d1c73d5a7d81f79
Benjamin Deconinck <bdbenboss@hotmail.com>
2022-04-21 19:23:21 +0200

    seed add

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -6,4 +6,15 @@ class RestaurantsController < ApplicationController
   def show
     @restaurant = Restaurant.find(params[:id])
   end
+
+  def create
+    @restaurant = Restaurant.new(params[:restaurant])
+    @restaurant.save
+  end
+
+  redirect_to restaurant_path(@restaurant)
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :category)
+  end
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,4 @@
 class Restaurant < ApplicationRecord
-  belongs_to :restaurant
-  validates :name, :adress, presence: true
-  validates :category, presence: true, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"] }
+  validates :name, :address, presence: true
+  validates :category, presence: true, inclusion: { in: %w[chinese italian japanese french belgian] }
 end
